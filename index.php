3ff311382e86ab7defcfc99b28baa6e46709a8a0
t1ffanyc <94410616+t1ffanyc@users.noreply.github.com>
2021-11-30 14:21:05 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="myCss.css">
     <title>Document</title>
 </head>
 <body>
-    
+<script type="text/javascript" src="Website.php"></script>
 </body>
 </html>
