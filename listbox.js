0cc6c972e62b2d8926b9a98cd09614bf9c0bb401
apaphy <82987703+apaphy@users.noreply.github.com>
2021-12-14 15:12:09 -0800

    problems overhaul

--- listbox.js
+++ listbox.js
@@ -1,3 +1,7 @@
-document.getElementById("sel").onclick = function(d){
-  window.location = this.value;
-};
+function putininfo(select)
+{
+  var info = select.options[select.selectedIndex].value;
+  var name = select.options[select.selectedIndex].text;
+  document.getElementById("problem-info").innerHTML = info;
+  document.getElementById("problem-name").innerHTML = name;
+}
