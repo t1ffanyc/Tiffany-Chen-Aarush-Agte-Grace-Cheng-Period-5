f23251b0e565f10c75c08bb03e3f481240709e35
gracc1 <94411710+gracc1@users.noreply.github.com>
2021-12-14 15:13:21 -0800

    register css

--- register.php
+++ register.php
@@ -100,9 +100,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="myCss.css">
     <style>
-        body{ font: 14px sans-serif; }
+        body{ font: 14px; }
         .wrapper{ width: 360px; padding: 20px; }
     </style>
 </head>
