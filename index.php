e9911c7fc902f69ae4b3594daa48230777903d20
t1ffanyc <94410616+t1ffanyc@users.noreply.github.com>
2021-12-14 15:02:47 -0800

    123

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <li><a href="problems.html">problems</a></li>
             <li><a href="characters.html">characters</a></li>
             <li><a href="quiz.html">quiz</a></li>
-            <li><a href="register.php">REGSITER</a></li>
+            <li><a href="register.php">register</a></li>
         </ul>
     </div>
 
