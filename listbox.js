2fea65da4855cbc482e0c26463acf6632451c468
apaphy <82987703+apaphy@users.noreply.github.com>
2021-12-14 22:40:41 -0800

    added functionality to problems page

--- listbox.js
+++
@@ -1,7 +0,0 @@
-function putininfo(select)
-{
-  var info = select.options[select.selectedIndex].value;
-  var name = select.options[select.selectedIndex].text;
-  document.getElementById("problem-info").innerHTML = info;
-  document.getElementById("problem-name").innerHTML = name;
-}
