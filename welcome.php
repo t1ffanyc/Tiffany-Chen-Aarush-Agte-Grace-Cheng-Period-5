0c650e7c616416b0e7cf744471c2b5666e8c0780
gracc1 <94411710+gracc1@users.noreply.github.com>
2021-12-15 21:14:39 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -13,14 +13,25 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="myCss.css">
+    <title>My Account</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <div class="titlebar">
+        <ul>
+            <li><a href="index.php">home</a></li>
+            <li><a href="problems.html">problems</a></li>
+            <li><a href="characters.html">characters</a></li>
+            <li><a href="quiz.html">quiz</a></li>
+            <li><a href="register.php">register</a></li>
+        </ul>
+    </div>
+<body>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. My Account </h1>
     <p>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
