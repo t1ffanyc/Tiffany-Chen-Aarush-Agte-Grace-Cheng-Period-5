0417cb7381f87b4bbe571c01925427b736cae327
gracc1 <94411710+gracc1@users.noreply.github.com>
2021-12-15 23:06:46 -0800

    sg

--- welcome.php
+++ welcome.php
@@ -13,7 +13,50 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive columns */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+    display: block;
+    margin-bottom: 20px;
+  }
+}
+
+/* Style the counter cards */
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  padding: 16px;
+  text-align: center;
+  background-color: #f1f1f1;
+}
+</style>
     <link rel="stylesheet" type="text/css" href="myCss.css">
     <title>My Account</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -31,10 +74,28 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         </ul>
     </div>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. My Account </h1>
-    <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
+<div class="row">
+  <div class="column">
+    <div class="card">
+    <img src="farmernhjo.png" alt="Avatar" style="width:100%">
+    <h4><h1 class="my-5">Welcome <?php echo htmlspecialchars($_SESSION["email"]); ?></h4> 
+    <p>Status: Moosaco friend!</p> 
+    </div>
+  </div>
+<div class="column">
+    <div class="card">
+      <h3>Information</h3>
+      <p><?php echo htmlspecialchars($_SESSION["password"]); ?></p>
+      <p>Some text</p>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <h3>Options</h3>
+      <p><a href="reset-password.php" class="btn btn-warning">Reset Your Password</a></p>
+      <p><a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a></p>
+    </div>
+  </div>
+</div>
 </body>
 </html>
