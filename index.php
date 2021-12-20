e802b83b328d899fe66c23388831f28410065512
apaphy <82987703+apaphy@users.noreply.github.com>
2021-12-19 23:42:02 -0800

    fixed slidehoshow buttons

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
         position: absolute;
         top: 50%;
         width: auto;
-        margin-top: 800px;
+        margin-top: 600px;
         margin-left: 35px;
         padding: 16px;
         color: white;
@@ -46,8 +46,8 @@
         position: absolute;
         top: 50%;
         width: auto;
-        margin-top: 800px;
-        margin-right: 225px;
+        margin-top: 600px;
+        margin-right: 350px;
         padding: 16px;
         color: white;
         font-weight: bold;
@@ -144,7 +144,7 @@
         <span
             class="txt-rotate"
             data-period="2000"
-            data-rotate='[ "cool.", "nerd.", "funny.", "scrumptious.", "absolutely delectable!" ]'></span>
+            data-rotate='[ "cool.", "nerdy.", "funny.", "scrumptious.", "absolutely delectable!" ]'></span>
         </h3>
         <h4 id = "animation-h2" class="animation">Do USACO now at: <a href="http://www.usaco.org/" target="_blank">usaco.org</a></h4>
     </div>
@@ -156,19 +156,19 @@
 
     <div class = "paragraph">
         <h2>When is USACO?</h2>
-        <p>The USACO competition takes place 3 times every school year, with a US Open competition after for all those who do well.</p>
+        <p>
+          The USACO competition takes place 3 times every school year, with a US Open competition after for all those who do well.
+          <br>
+
+        </p>
     </div>
-    
-    <div class="header">
-      <h1 class = "typed-out">characters</h1>
-  </div>
 
     <div class="slideshow-container">
         <div class="mySlides fade">
           <div class="numbertext">1 / 12</div>
           <figure>
             <img src="https://bloximages.newyork1.vip.townnews.com/daily-journal.com/content/tncms/assets/v3/editorial/c/53/c539e176-f4d0-11eb-9dba-579fe94fec4d/610a03e268fce.image.jpg?resize=1200%2C800" style="width:100%">
-            <figcaption>FARMER JOHN</figcaption>
+            <figcaption>Farmer John is the leader of the farm. He takes care of the cows, grows the crops, and hangs out. He's old-fashioned, old, but still a smart cookie and is a fantastic farmhead.</figcaption>
           </figure>
         </div>
       
@@ -176,7 +176,7 @@
           <div class="numbertext">2 / 12</div>
           <figure>
             <img src="https://pbs.twimg.com/profile_images/2344933022/cow2_400x400.jpg" style="width:100%">
-            <figcaption>BESSIE</figcaption>
+            <figcaption>Bessie the cow is the prize cow of the farm. She is the leader of the cows and the older sister to Elsie. She's well read and has an endless thirst for knowledge. She even has a college degree at such a young age!</figcaption>
           </figure>
         </div>
       
@@ -184,7 +184,7 @@
           <div class="numbertext">3 / 12</div>
           <figure>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm9rzbRGMbE0QsnLt8W8-8XrJSIjYgTuHGQw&usqp=CAU" style="width:100%">
-            <figcaption>ELSIE</figcaption>
+            <figcaption>Elsie is one of the younger cows, and Bessie's younger sister. She's adventurous, brave, and feisty. She is often seen playing games with the older cows. She loses frequently, but that doesn't deter her.</figcaption>
           </figure>
         </div>
 
@@ -192,7 +192,7 @@
             <div class="numbertext">4 / 12</div>
             <figure>
               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2y0SDs_xguuWBlZEJFw7IRFIbOQ0yfVYqag&usqp=CAU" style="width:100%">
-              <figcaption>MILDRED: Mildred is a vegan. She’s picky about food, so she understands being picky about the milk you drink.</figcaption>
+              <figcaption>Mildred is one of the older cows. From being on the farm so long, She has lots of life advice to give to the younger cows on the farm. She can often be seen relaxing in the shade approaching retirement.</figcaption>
             </figure>
             </div>
         
@@ -200,7 +200,7 @@
             <div class="numbertext">5 / 12</div>
             <figure>
               <img src="vpw2.jfif" style="width:100%">
-              <figcaption>BUTTERCUP</figcaption>
+              <figcaption>Buttercup is an energetic young squirt. She enjoys playing running games like tag and hide and seek. She is often seen playing with Elsie.</figcaption>
             </figure>
           </div>
         
@@ -208,7 +208,7 @@
             <div class="numbertext">6 / 12</div>
             <figure>
               <img src="https://assets.podomatic.net/ts/2a/46/fb/taylorsara/640x640_1150552.jpg?1539518516" style="width:100%">
-              <figcaption>BELINDA</figcaption>
+              <figcaption>Belinda is a motherly figure to many of the cows. She inspires them to do better in life and to pursue their futures. She also helps take care of their emotional needs.</figcaption>
             </figure>
           </div>
 
@@ -216,7 +216,7 @@
             <div class="numbertext">7 / 12</div>
             <figure>
               <img src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/beatrice-jenn-teel.jpg" style="width:100%">
-              <figcaption>BEATRICE</figcaption>
+              <figcaption>Beatrice is the mean girl of the group. She spreads rumors around many of her fellow cows due to her insecurities. She has a long way ahead of her for true redemption.</figcaption>
             </figure>
           </div>
         
@@ -224,7 +224,7 @@
             <div class="numbertext">8 / 12</div>
             <figure>
               <img src="https://i.ytimg.com/vi/kfK-N7YCp7Q/maxresdefault.jpg" style="width:100%">
-              <figcaption>BELLA: Inspirational Story of Bella the COW who Escaped a Tiny Stable<bR>On July 21, 2010 a truck loaded with dairy cows whose milk production was no longer cost effective was headed for a slaughterhouse.</figcaption>
+              <figcaption>Bella is beatrice's sidekick. She doesn't fuly understand why, but she follows Beatrice's every command. She tries to be everyone's friend, though.</figcaption>
             </figure>
           </div>
         
@@ -232,7 +232,7 @@
             <div class="numbertext">9 / 12</div>
             <figure>
               <img src="blue.jfif" style="width:100%">
-              <figcaption>BLUE</figcaption>
+              <figcaption>Blue is the clumsiest of the cows. She is young but eager, and often trips over herself. She is relentlessly made fun of, and cries a lot. However, she is extremely funny and makes friends that way.</figcaption>
             </figure>
           </div>
 
@@ -240,7 +240,7 @@
             <div class="numbertext">10 / 12</div>
             <figure>
               <img src="https://pbs.twimg.com/profile_images/530086650635112450/_NXzicMP_400x400.jpeg" style="width:100%">
-              <figcaption>BETSY</figcaption>
+              <figcaption>Betsy is a lovable old cow. She is extremely friendsly and makes friends with all the new cows on the farm. She is a model cow that everyone loves.</figcaption>
             </figure>
           </div>
         
@@ -248,14 +248,14 @@
             <div class="numbertext">11 / 12</div>
             <figure>
             <img src="https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2JlZWNkNjVhZTJmYzQ0NWRlZl80ODE2NzIyODkzXzIxYjc2ZjNlMzhfYi5qcGciXSxbInAiLCJjb252ZXJ0IiwiIl0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdLFsicCIsInRodW1iIiwiNzgweDUyMCMiXV0" style="width:100%">
-            <figcaption>SUE</figcaption>
+            <figcaption>Sue is the kook of the farm. She often forgets details about her life and is the older sister of Blue.</figcaption>
             </figure>
           </div>
           <div class="mySlides fade">
             <div class="numbertext">12 / 12</div>
             <figure>
             <img src="farmernhjo.png" style="width:100%">
-            <figcaption>FARMER NHOJ</figcaption>
+            <figcaption>Farmer Nhoj is an experimenter. He loves to tinker around with inventions in his shed. He and his natural enemy (and brother) Farmer John frequently have discussions (and arguments) about farming methods </figcaption>
             </figure>
           </div>
     
