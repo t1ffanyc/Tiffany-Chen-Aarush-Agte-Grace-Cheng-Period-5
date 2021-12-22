67b5212a35f1a0a5d3628b2b165382ca3a5d70f9
t1ffanyc <94410616+t1ffanyc@users.noreply.github.com>
2021-12-22 10:01:36 -0800

    Create creationscript.sql

---
+++ creationscript.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE logindb;
+Use logindb; 
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    cows VARCHAR (255)
+);
+SELECT * FROM Users;
